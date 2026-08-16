7e810e81bdfeed7e2d9adb079f7f49dc18e88167
nguyenphuochao <nguyenphuochao123@gmail.com>
2026-08-16 14:37:30 +0700

    feat: setup docker hub image

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
+const API_DEVELOPMENT = "http://localhost:8000/api";
+const API_PRODUCTION = "http://103.163.118.135:8000/api";
+
 const BASE_URL =
-  import.meta.env.MODE === "development" ? "http://localhost:8000/api" : "http://localhost:8000/api";
+  import.meta.env.MODE === "development" ? API_DEVELOPMENT : API_PRODUCTION;
 
 const api = axios.create({
   baseURL: BASE_URL,
